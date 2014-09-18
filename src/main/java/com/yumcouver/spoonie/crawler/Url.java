5e33a7d94e1fe3560d00236b118f3ec72be364db
Torres9 <Torres9Jin@gmail.com>
2014-09-17 18:16:29 -0700

    added Url and UrlPool classes

--- src/main/java/com/yumcouver/spoonie/crawler/Url.java
+++ src/main/java/com/yumcouver/spoonie/crawler/Url.java
@@ -1,16 +1,38 @@
 package com.yumcouver.spoonie.crawler;
 
+import java.net.InetAddress;
 import java.net.MalformedURLException;
 import java.net.URL;
 
 class Url {
 
-    private URL url;
+    private URL ipUrl = null;
+    private URL domainUrl = null;
 
     Url(String url) {
         try {
-            this.url = new URL(url);
+            this.domainUrl = new URL(url);
         } catch (MalformedURLException e) {
+        }
+        if (this.domainUrl == null) {
+            try {
+                this.domainUrl = new URL("http://" + url);
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+        }
+        if (this.domainUrl.getPort() == -1) {
+            try {
+                this.domainUrl = new URL(this.domainUrl.getProtocol(), this.domainUrl.getHost(), this.domainUrl.getDefaultPort(), this.domainUrl.getFile());
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+        }
+        InetAddress address = null;
+        try {
+            address = InetAddress.getByName(domainUrl.getHost());
+            this.ipUrl = new URL(this.domainUrl.getProtocol(), address.getHostAddress(), this.domainUrl.getPort(), this.domainUrl.getFile());
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
@@ -18,5 +40,11 @@ class Url {
     public boolean match(String regex) {
 //        TODO fill in later
         return true;
+    }
+
+    @Override
+    public String toString() {
+//        TODO change it later
+        return this.ipUrl.toString() + "<--:-->" + this.domainUrl.toString();
     }
 }
--- src/main/java/com/yumcouver/spoonie/crawler/UrlPool.java
+++ src/main/java/com/yumcouver/spoonie/crawler/UrlPool.java
@@ -1,4 +1,41 @@
 package com.yumcouver.spoonie.crawler;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+
 public class UrlPool {
+    private static final Log log = LogFactory.getLog(UrlPool.class);
+
+    private static UrlPool ourInstance = new UrlPool();
+
+    public static UrlPool getInstance() {
+        return ourInstance;
+    }
+
+    private LinkedList<Url> queue;
+
+    private UrlPool() {
+        this.queue = new LinkedList<Url>();
+    }
+
+    public List<Url> fetchUrls(int length) {
+        synchronized (this.queue) {
+            log.info("Fetching " + String.valueOf(Math.min(this.queue.size(), length)) + " urls ...");
+            List<Url> urls = new ArrayList<Url>(this.queue.subList(0, Math.min(this.queue.size(), length)));
+            this.queue.removeAll(urls);
+            return urls;
+        }
+    }
+
+    public void addUrls(Collection<Url> urls) {
+        synchronized (this.queue) {
+            log.info("Adding " + String.valueOf(urls.size()) + " urls ...");
+            this.queue.addAll(urls);
+        }
+    }
 }
